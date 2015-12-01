b8cb2cc9d7333842db980818feed202a9bac1b17
Ivo Georgiev <ivo@linvo.org>
2015-12-01 13:08:04 +0200

    add tape to deps, added test command

---
+++ test/basic.js
@@ -0,0 +1,7 @@
+var tape = require("tape");
+var fs = require("fs");
+
+fs.readdirSync(".").filter(function(x){ return x.match(".json$") && x.match("-") }).forEach(function(lang) {
+	tape(lang, function(t) {
+	})
+});
