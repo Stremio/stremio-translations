6687c40e1e94fd4c15c2ac4ba4140b0f26f80159
Ivo Georgiev <ivo@linvo.org>
2015-12-01 13:15:40 +0200

    Add basic parsing test

--- test/basic.js
+++ test/basic.js
@@ -3,5 +3,7 @@ var fs = require("fs");
 
 fs.readdirSync(".").filter(function(x){ return x.match(".json$") && x.match("-") }).forEach(function(lang) {
 	tape(lang, function(t) {
+		try { require("../"+lang); t.ok(true, "parses properly"); } catch(e) { t.error(e) };
+		t.end();
 	})
 });
