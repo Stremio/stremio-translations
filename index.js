22e2c998f7d0f1cb990092e0a7d0e04832ba1c33
Vladimir Borisov <borisov1024@gmail.com>
2019-02-12 13:42:05 +0200

    Updated language list

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // No automatic generation: we don't want to depend on fs, as we want to keep this universal
 // ls *-*.json | while read line; do printf "\"`printf $line | cut -d '.' -f1`\": require(\"./$line\"),\n"; done
-module.exports = function() { 
+module.exports = function() {
 	return {
-		"bg-BG": require("./bg-BG.json"),
+        "bg-BG": require("./bg-BG.json"),
 		"da-DK": require("./da-DK.json"),
 		"de-DE": require("./de-DE.json"),
+		"el-GR": require("./el-GR.json"),
 		"en-US": require("./en-US.json"),
 		"es-ES": require("./es-ES.json"),
 		"fr-FR": require("./fr-FR.json"),
@@ -17,12 +18,13 @@ module.exports = function() {
 		"nb-NO": require("./nb-NO.json"),
 		"nl-NL": require("./nl-NL.json"),
 		"nn-NO": require("./nn-NO.json"),
+		"pl-PL": require("./pl-PL.json"),
 		"pt-BR": require("./pt-BR.json"),
 		"pt-PT": require("./pt-PT.json"),
 		"ru-RU": require("./ru-RU.json"),
 		"se-SE": require("./se-SE.json"),
 		"sr-RS": require("./sr-RS.json"),
-		"tr-TR": require("./tr-TR.json"),
+		"tr-TR": require("./tr-TR.json")
 	}
 };
 
@@ -31,6 +33,7 @@ module.exports.all = [
 	"bg-BG",
 	"da-DK",
 	"de-DE",
+	"el-GR",
 	"en-US",
 	"es-ES",
 	"fr-FR",
@@ -43,10 +46,11 @@ module.exports.all = [
 	"nb-NO",
 	"nl-NL",
 	"nn-NO",
+	"pl-PL",
 	"pt-BR",
 	"pt-PT",
 	"ru-RU",
 	"se-SE",
 	"sr-RS",
-	"tr-TR",
+	"tr-TR"
 ];
