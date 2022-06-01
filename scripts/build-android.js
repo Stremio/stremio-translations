02414fc9cdeb5edf703d9324832312a93a9b1643
Alexandru Branza <branza.alex@gmail.com>
2022-06-01 14:59:34 +0300

    Set Region Correctly in Android Build Folder Name

--- scripts/build-android.js
+++ scripts/build-android.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 var path = require('path');
 var fs = require('fs');
 
@@ -7,13 +5,15 @@ var files = fs.readdirSync(process.cwd());
 
 fs.mkdirSync(path.join(process.cwd(), 'build'), { recursive: true });
 
-var defaultLang = 'en-US'
+var defaultLang = 'en-rUS'
 
 files.forEach(function(file) {
 	if (/[a-z][a-z]-[A-Z][A-Z]\.json$/g.test(file)) {
-		var lang = file.replace('.json', '');
+		var lang = file.split('-')[0];
+		var region = file.split('-')[1].replace('.json', '');
+		var langTag = lang + '-r' + region;
 		var translations = JSON.parse(fs.readFileSync(path.join(process.cwd(), file)))
-		var langFolder = 'values' + (lang !== defaultLang ? '-' + lang : '')
+		var langFolder = 'values' + (langTag !== defaultLang ? '-' + langTag : '')
 		fs.mkdirSync(path.join(process.cwd(), 'build', langFolder), { recursive: true });
 		let xml = '<?xml version="1.0" encoding="utf-8"?>\n';
 		xml += '<resources>\n';
