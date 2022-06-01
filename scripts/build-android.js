c6213fd635f5796f874bebacc8d546b6898ca974
Alexandru Branza <branza.alex@gmail.com>
2022-06-01 14:34:20 +0300

    Use Full Locale in Android Build Folder Name

--- scripts/build-android.js
+++ scripts/build-android.js
@@ -9,11 +9,11 @@ fs.mkdirSync(path.join(process.cwd(), 'build'), { recursive: true });
 
 files.forEach(function(file) {
 	if (/[a-z][a-z]-[A-Z][A-Z]\.json$/g.test(file)) {
-		var lang = file.split('-')[0];
+		var lang = file.replace('.json', '');
 		var translations = JSON.parse(fs.readFileSync(path.join(process.cwd(), file)))
 		fs.mkdirSync(path.join(process.cwd(), 'build', 'values-' + lang), { recursive: true });
 		let xml = '<?xml version="1.0" encoding="utf-8"?>\n';
-		xml += '<resources xmlns:xliff="urn:oasis:names:tc:xliff:document:1.2">\n';
+		xml += '<resources>\n';
 		Object.keys(translations).forEach(key => {
 			xml += '  <string name="'+key.toLowerCase()+'">'+translations[key]+'</string>\n';
 		})
