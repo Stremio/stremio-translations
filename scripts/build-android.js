96c9a04e293ffa28f6a6d4e2611f99931a860ebf
Alexandru Branza <branza.alex@gmail.com>
2022-06-01 14:52:09 +0300

    Set `en-US` as Default Language Folder for Android

--- scripts/build-android.js
+++ scripts/build-android.js
@@ -7,17 +7,20 @@ var files = fs.readdirSync(process.cwd());
 
 fs.mkdirSync(path.join(process.cwd(), 'build'), { recursive: true });
 
+var defaultLang = 'en-US'
+
 files.forEach(function(file) {
 	if (/[a-z][a-z]-[A-Z][A-Z]\.json$/g.test(file)) {
 		var lang = file.replace('.json', '');
 		var translations = JSON.parse(fs.readFileSync(path.join(process.cwd(), file)))
-		fs.mkdirSync(path.join(process.cwd(), 'build', 'values-' + lang), { recursive: true });
+		var langFolder = 'values' + (lang !== defaultLang ? '-' + lang : '')
+		fs.mkdirSync(path.join(process.cwd(), 'build', langFolder), { recursive: true });
 		let xml = '<?xml version="1.0" encoding="utf-8"?>\n';
 		xml += '<resources>\n';
 		Object.keys(translations).forEach(key => {
 			xml += '  <string name="'+key.toLowerCase()+'">'+translations[key].replace(/{{[^}]+}}/gm, '%s')+'</string>\n';
 		})
 		xml += '</resources>';
-		fs.writeFileSync(path.join(process.cwd(), 'build', 'values-' + lang, 'strings.xml'), xml);
+		fs.writeFileSync(path.join(process.cwd(), 'build', langFolder, 'strings.xml'), xml);
 	}
 })
