c5928f70648d35c103e39aae116c53c40f82084e
Alexandru Branza <branza.alex@gmail.com>
2022-06-01 14:46:25 +0300

    Use `%s` For Text Vars for Android

--- scripts/build-android.js
+++ scripts/build-android.js
@@ -15,7 +15,7 @@ files.forEach(function(file) {
 		let xml = '<?xml version="1.0" encoding="utf-8"?>\n';
 		xml += '<resources>\n';
 		Object.keys(translations).forEach(key => {
-			xml += '  <string name="'+key.toLowerCase()+'">'+translations[key]+'</string>\n';
+			xml += '  <string name="'+key.toLowerCase()+'">'+translations[key].replace(/{{[^}]+}}/gm, '%s')+'</string>\n';
 		})
 		xml += '</resources>';
 		fs.writeFileSync(path.join(process.cwd(), 'build', 'values-' + lang, 'strings.xml'), xml);
