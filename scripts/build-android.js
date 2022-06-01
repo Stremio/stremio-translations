b056b04aebba25b775f8e3c3f94d94ff0545904b
Alexandru Branza <branza.alex@gmail.com>
2022-06-01 18:20:44 +0300

    Semicolons on Line End

--- scripts/build-android.js
+++ scripts/build-android.js
@@ -20,11 +20,11 @@ function str2xml(str) {
 		'\\"': '\\\\\\"', // escape quotes
 		'\\?': '\\\\\\?', // escape question marks
 		'<[^>]*>?': '', // strip html elements
-	}
+	};
 
-	textVars.forEach(el => { str = str.replace(new RegExp(el, 'gm'), '%s') })
-	newLines.forEach(el => { str = str.replace(new RegExp(el, 'gm'), '\n') })
-	Object.keys(replacers).forEach(key => { str = str.replace(new RegExp(key, 'gm'), replacers[key]) })
+	textVars.forEach(el => { str = str.replace(new RegExp(el, 'gm'), '%s') });
+	newLines.forEach(el => { str = str.replace(new RegExp(el, 'gm'), '\n') });
+	Object.keys(replacers).forEach(key => { str = str.replace(new RegExp(key, 'gm'), replacers[key]) });
 	return str;
 }
 
@@ -50,11 +50,11 @@ files.forEach(function(file) {
 		fs.mkdirSync(path.join(process.cwd(), 'build', langFolder), { recursive: true });
 		var xml = '<?xml version="1.0" encoding="utf-8"?>\n';
 		xml += '<resources>\n';
-		var dedup = {}
+		var dedup = {};
 		Object.keys(translations).filter(key => slug(key)).forEach(key => dedup[slug(key.toLowerCase())] = translations[key]);
 		Object.keys(dedup).forEach(key => {
 			xml += '  <string name="'+key+'">'+str2xml(dedup[key])+'</string>\n';
-		})
+		});
 		xml += '</resources>';
 		fs.writeFileSync(path.join(process.cwd(), 'build', langFolder, 'strings.xml'), xml);
 	}
