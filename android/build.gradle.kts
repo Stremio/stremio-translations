fde98600c82c3f9c2c3b004b234acbdd6d7e780b
Tim <tymmesyde@gmail.com>
2026-01-29 13:18:56 +0100

    build(android): update java version

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         compileSdk = 34
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
